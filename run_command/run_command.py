c0402ca6d417f871eb6fe82377e3af70532bf12a
jakeogh <github.com@v6y.net>
2022-10-23 11:55:24 -0700

    auto-commit

--- run_command/run_command.py
+++ run_command/run_command.py
@@ -62,7 +62,7 @@ def run_command(
     ask: bool = False,
 ):
 
-    ic(command)
+    epprint(command)
     maxone(
         [popen, interactive, system],
         msg="--popen --interactive and --system are mutually exclusive",
